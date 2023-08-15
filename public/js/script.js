e9ca7e530ec03ff331d5d6aabe685a930651d0ff
AnotheerDev <leger.julien94@gmail.com>
2023-08-15 18:14:23 +0200

    adding addEventListener for parallax animation

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,15 @@
+// Menu burger
 function menuOnClick() {
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+// Parallax
+
+window.addEventListener('scroll', function() {
+    var parallax = document.querySelector('.parallax-section');
+    var scrollPosition = window.scrollY; // Utilisez 'scrollY' à la place de 'pageYOffset'
+
+    parallax.style.backgroundPosition = 'center ' + (scrollPosition * 0,1) + 'px';
+});
