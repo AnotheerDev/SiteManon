a2d3cd17aca6925c4f014c59db7149b973327b7f
AnotheerDev <leger.julien94@gmail.com>
2023-10-10 19:59:38 +0200

    création article via easyadmin

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ArticleCrudController extends AbstractCrudController
@@ -12,14 +14,16 @@ class ArticleCrudController extends AbstractCrudController
         return Article::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            // ne pas afficher l'id et la date car si je dois créer un article je veux que l'id et la date soit généré automatiquement
+            // IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('content'),
+            ImageField::new('photo')->setBasePath('img/')->setUploadDir('public/img/'),
         ];
     }
-    */
+
 }
