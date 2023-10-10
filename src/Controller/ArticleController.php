8e3e5aa35f5d3c03bf0afa4abeb18b7795511999
AnotheerDev <leger.julien94@gmail.com>
2023-10-10 21:10:41 +0200

    add articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    // public function index(): Response
+    // {
+    //     return $this->render('article/index.html.twig', [
+    //         'controller_name' => 'ArticleController',
+    //     ]);
+    // }
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findAllArticles();
+
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'articles' => $articles,
         ]);
     }
 }
