0b882a61aafd603c2d0a3b674a5203fd6235cf0e
AnotheerDev <leger.julien94@gmail.com>
2023-10-14 00:27:03 +0200

    modif function contact to send the form

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactRequest;
 use App\Form\ContactRequestType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,9 +21,20 @@ class NewsletterController extends AbstractController
     }
 
     #[Route('/newsletter/contact', name: 'app_contact')]
-    public function contact(): Response
+    public function contact(ManagerRegistry $doctrine, Request $request): Response
     {
+        $contactRequest = new ContactRequest();
         $form = $this->createForm(ContactRequestType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactRequest = $form->getData();
+            $em = $doctrine->getManager();
+            $em->persist($contactRequest);
+            $em->flush();
+            $this->addFlash('success', 'Votre demande a bien été enregistrée');
+            return $this->redirectToRoute('app_contact');
+        }
+
         return $this->render('newsletter/contact.html.twig', [
             'form' => $form->createView(),
         ]);
