b67a8c0b3002f0419f8feca6378496ee50319122
AnotheerDev <leger.julien94@gmail.com>
2023-10-13 23:37:31 +0200

    Adding ContactRequestType form

---
+++ src/Form/ContactRequestType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ContactRequest;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactRequestType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastName',TextType::class)
+            ->add('firstName',TextType::class)
+            ->add('email',EmailType::class)
+            ->add('subject',TextType::class)
+            ->add('message', TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContactRequest::class,
+        ]);
+    }
+}
