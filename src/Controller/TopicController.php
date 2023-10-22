67398d8772449e985c7cbee4bf56598994640e94
AnotheerDev <leger.julien94@gmail.com>
2023-10-22 23:42:34 +0200

    add function topics

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TopicRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TopicController extends AbstractController
 {
@@ -13,6 +15,28 @@ class TopicController extends AbstractController
     {
         return $this->render('topic/index.html.twig', [
             'controller_name' => 'TopicController',
+        ]);
+    }
+
+
+    #[Route('/topic/{categoryId}', name: 'app_topics')]
+
+    public function topics(int $categoryId, CategoryRepository $categoryRepository, TopicRepository $topicRepository): Response
+    {
+        // Récupérer l'objet Category depuis la base de données
+        $category = $categoryRepository->find($categoryId);
+    
+        if (!$category) {
+            throw $this->createNotFoundException('La catégorie demandée n\'existe pas');
+        }
+    
+        // Récupérer les topics liés à cette catégorie
+        $topics = $topicRepository->findTopicsByCategory($categoryId);
+    
+        // Passer les topics et le nom de la catégorie à la vue
+        return $this->render('topic/index.html.twig', [
+            'topics' => $topics,
+            'categoryName' => $category->getName(), // Ici l'objet Category pour obtenir le nom
         ]);
     }
 }
