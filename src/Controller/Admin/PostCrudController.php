74b01bfcce661d08bc132bb28f64d42e40fe3d40
AnotheerDev <leger.julien94@gmail.com>
2023-10-22 21:55:11 +0200

    add custom Field for PostCrudController

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PostCrudController extends AbstractCrudController
@@ -12,14 +15,15 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            AssociationField::new('user'),
+            AssociationField::new('topicPost'),
+            DateField::new('dateCreation'),
+            TextEditorField::new('content'),
         ];
     }
-    */
+
 }
