1151a3387466ffe9b38f67fed80c3534268ed6bf
AnotheerDev <leger.julien94@gmail.com>
2023-10-31 09:12:05 +0100

    modif function carousel adding if to avoid error

--- public/js/script.js
+++ public/js/script.js
@@ -11,80 +11,48 @@ window.addEventListener('scroll', function() {
     var parallax = document.querySelector('.parallax-section');
     var scrollPosition = window.scrollY; // Utilise 'scrollY' à la place de 'pageYOffset'
 
-    parallax.style.backgroundPosition = 'center ' + (scrollPosition * 0,1) + 'px';
+    parallax.style.backgroundPosition = 'center ' + (scrollPosition * 0.1) + 'px';
 });
 
 
 // Carousel
 
 
-
 document.addEventListener("DOMContentLoaded", function() {
     const carouselInner = document.querySelector(".carousel-inner");
     const carouselItems = document.querySelectorAll(".carousel-item");
-    // const prevButton = document.querySelector(".prev-button");
-    // const nextButton = document.querySelector(".next-button");
     const carouselButtonsContainer = document.querySelector(".carousel-buttons");
 
-    let currentIndex = 0;
-    let interval;
+    if (carouselInner && carouselItems.length > 0 && carouselButtonsContainer) {
+        let currentIndex = 0;
+        let interval;
 
-    carouselItems.forEach((item, index) => {
-        const button = document.createElement("button");
-        button.className = "carousel-button";
-        if (index === 0) {
-            button.classList.add("active");
+        function updateCarousel() {
+            carouselInner.style.transform = `translateX(-${currentIndex * 25}%)`;
         }
-        button.addEventListener("click", () => {
-            currentIndex = index;
-            updateCarousel();
-            updateButtons();
-            resetInterval();
-        });
-        carouselButtonsContainer.appendChild(button);
-    });
 
-    // prevButton.addEventListener("click", () => {
-    //     currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
-    //     updateCarousel();
-    //     updateButtons();
-    //     resetInterval();
-    // });
+        function updateButtons() {
+            carouselButtonsContainer.querySelectorAll(".carousel-button").forEach((button, index) => {
+                if (index === currentIndex) {
+                    button.classList.add("active");
+                } else {
+                    button.classList.remove("active");
+                }
+            });
+        }
 
-    // nextButton.addEventListener("click", () => {
-    //     currentIndex = (currentIndex + 1) % carouselItems.length;
-    //     updateCarousel();
-    //     updateButtons();
-    //     resetInterval();
-    // });
+        function resetInterval() {
+            clearInterval(interval);
+            interval = setInterval(() => {
+                currentIndex = (currentIndex + 1) % carouselItems.length;
+                updateCarousel();
+                updateButtons();
+            }, 5000);
+        }
 
-    function updateCarousel() {
-        carouselInner.style.transform = `translateX(-${currentIndex * 25}%)`;
-    }
-
-    function updateButtons() {
-        carouselButtonsContainer.querySelectorAll(".carousel-button").forEach((button, index) => {
-            if (index === currentIndex) {
-                button.classList.add("active");
-            } else {
-                button.classList.remove("active");
-            }
-        });
-    }
-
-    interval = setInterval(() => {
-        currentIndex = (currentIndex + 1) % carouselItems.length;
+        // Initialisation du carrousel
         updateCarousel();
         updateButtons();
-    }, 5000);
-
-    function resetInterval() {
-        clearInterval(interval);
-        interval = setInterval(() => {
-            currentIndex = (currentIndex + 1) % carouselItems.length;
-            updateCarousel();
-            updateButtons();
-        }, 5000);
+        resetInterval();
     }
 });
-
