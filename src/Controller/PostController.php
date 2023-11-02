d1b25f6bd1a2ddf2f8c352c761520e2eb61514dc
AnotheerDev <leger.julien94@gmail.com>
2023-11-02 14:46:50 +0100

    add function posts

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PostRepository;
+use App\Repository\TopicRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
@@ -15,4 +17,26 @@ class PostController extends AbstractController
             'controller_name' => 'PostController',
         ]);
     }
+
+    #[Route('/post/{topicId}', name: 'app_posts')]
+    public function posts(int $topicId, TopicRepository $topicRepository, PostRepository $postRepository): Response
+    {
+        // Récupérer l'objet Topic depuis la base de données
+        $topic = $topicRepository->find($topicId);
+    
+        if (!$topic) {
+            throw $this->createNotFoundException('Le topic demandé n\'existe pas');
+        }
+    
+        // Récupérer les posts liés à ce topic
+        $posts = $postRepository->findPostsByTopic($topicId);
+    
+        // Passer les posts et le nom du topic à la vue
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts,
+            'topicName' => $topic->getName(), // Ici on utilise l'objet Topic pour obtenir le nom
+        ]);
+    }
+    
+    
 }
