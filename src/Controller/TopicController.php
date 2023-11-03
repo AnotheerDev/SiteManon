e16b8fa0cb8d8b7ace0564a699332da443e83c25
AnotheerDev <leger.julien94@gmail.com>
2023-11-03 10:56:59 +0100

    Add topic creation functionality to TopicController

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Topic;
+use App\Form\TopicType;
 use App\Repository\TopicRepository;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,9 +24,15 @@ class TopicController extends AbstractController
     }
 
 
-    #[Route('/topic/{categoryId}', name: 'app_topics')]
+    private $security;
 
-    public function topics(int $categoryId, CategoryRepository $categoryRepository, TopicRepository $topicRepository): Response
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    #[Route('/topic/{categoryId}', name: 'app_topics')]
+    public function topics(int $categoryId, CategoryRepository $categoryRepository, TopicRepository $topicRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
         // Récupérer l'objet Category depuis la base de données
         $category = $categoryRepository->find($categoryId);
@@ -30,14 +41,34 @@ class TopicController extends AbstractController
             throw $this->createNotFoundException('La catégorie demandée n\'existe pas');
         }
     
+        // Créer un nouveau topic et le formulaire associé
+        $topic = new Topic();
+        $form = $this->createForm(TopicType::class, $topic);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->security->getUser(); // Récupérer l'utilisateur connecté
+            $topic->setUser($user); // Associer l'utilisateur au topic
+            $topic->setDateCreation(new \DateTime()); // Définir la date de création
+            $topic->setLocked(false); // Définir le statut verrouillé à false par défaut
+            $topic->setCategoryTopic($category); // Associer la catégorie au topic
+    
+            $entityManager->persist($topic);
+            $entityManager->flush();
+    
+            // Redirection ou affichage d'un message de succès
+            return $this->redirectToRoute('app_posts', ['topicId' => $topic->getId()]);
+        }
+    
         // Récupérer les topics liés à cette catégorie
         $topics = $topicRepository->findTopicsByCategory($categoryId);
     
-        // Passer les topics et le nom de la catégorie à la vue
+        // Passer les topics, le nom de la catégorie et le formulaire à la vue
         return $this->render('topic/index.html.twig', [
             'topics' => $topics,
             'categoryName' => $category->getName(), // Ici l'objet Category pour obtenir le nom
             'categories' => $categoryRepository->findAll(),
+            'form' => $form->createView(), // Ajouter cette ligne pour passer le formulaire à la vue
         ]);
     }
 }
