9eae207d99d1b4cb7d89f068e940b176eb46dd58
AnotheerDev <leger.julien94@gmail.com>
2023-11-04 23:45:04 +0100

    Add most clicked topics to category index page.

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Repository\TopicRepository;
 use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, TopicRepository $topicRepository): Response
     {
         $categories = $categoryRepository->findAllCategories();
+        $mostClickedTopics = $topicRepository->findMostClickedTopics();
+
 
         return $this->render('category/index.html.twig', [
             'categories' => $categories,
+            'mostClickedTopics' => $mostClickedTopics,
         ]);
     }
 }
