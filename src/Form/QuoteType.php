67242f7b714037fc9199dd0267e241dea030d9c5
AnotheerDev <leger.julien94@gmail.com>
2023-11-06 16:44:21 +0100

    Add QuoteType form for creating new quotes

---
+++ src/Form/QuoteType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Quote;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class QuoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            // ->add('dateCreation')
+            ->add('content', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Entrez votre commentaire'
+                ]
+                ])
+            // ->add('user')
+            // ->add('article')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Quote::class,
+        ]);
+    }
+}
