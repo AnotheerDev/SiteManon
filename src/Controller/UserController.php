86fe4aa4934500bac98bbd80287608a0afe2a5a9
AnotheerDev <leger.julien94@gmail.com>
2023-11-07 11:44:40 +0100

    Refactor UserController to safely delete user and disconnect related entities.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
@@ -15,4 +17,28 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
+
+
+    public function deleteUser(User $user, EntityManagerInterface $entityManager): void
+    {
+        // Parcourir tous les commentaires de l'utilisateur et les déconnecter de l'utilisateur
+        foreach ($user->getUserQuotes() as $quote) {
+            $quote->setUser(null);
+        }
+
+        // Parcourir tous les posts de l'utilisateur et les déconnecter de l'utilisateur
+        foreach ($user->getCreatPost() as $post) {
+            $post->setUser(null);
+        }
+
+        // Parcourir tous les topics de l'utilisateur et les déconnecter de l'utilisateur
+        foreach ($user->getCreatTopic() as $topic) {
+            $topic->setUser(null);
+        }
+
+        // Maintenant, vous pouvez supprimer l'utilisateur en toute sécurité
+        $entityManager->remove($user);
+        $entityManager->flush();
+    }
+
 }
