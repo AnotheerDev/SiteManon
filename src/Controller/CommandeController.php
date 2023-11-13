6c1d94e4b6a60d6dd0cc61f7ed24927603d2de65
AnotheerDev <leger.julien94@gmail.com>
2023-11-13 15:27:28 +0100

    Add PDF download functionality for orders

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use App\Entity\Commande;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CommandeController extends AbstractController
 {
@@ -14,5 +18,43 @@ class CommandeController extends AbstractController
         return $this->render('commande/index.html.twig', [
             'controller_name' => 'CommandeController',
         ]);
+    }
+
+    #[Route('/download_order_pdf/{id}', name: 'download_order_pdf')]
+    public function downloadOrderPdf($id, EntityManagerInterface $entityManager): Response
+    {
+        $commande = $entityManager->getRepository(Commande::class)->find($id);
+
+        if (!$commande) {
+            throw $this->createNotFoundException('La commande n\'a pas été trouvée.');
+        }
+
+        $total = 0;
+        foreach ($commande->getCarts() as $cart) {
+            $total += $cart->getProduct()->getPrice() * $cart->getQuantity();
+        }
+
+        // Récupérer le HTML à partir du template Twig
+        $html = $this->renderView('checkout/order_pdf.html.twig', [
+            'commande' => $commande,
+            'total' => $total,
+        ]);
+
+        // Configurer DomPDF
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        
+        $dompdf = new Dompdf($pdfOptions);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        // Envoyer le PDF au navigateur
+        $pdfOutput = $dompdf->output();
+        $response = new Response($pdfOutput);
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Content-Disposition', 'attachment; filename="commande_' . $id . '.pdf"');
+
+        return $response;
     }
 }
