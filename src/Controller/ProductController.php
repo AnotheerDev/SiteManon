ea466c64f160b827a373fd2efc1ff85fd3a6d8ba
AnotheerDev <leger.julien94@gmail.com>
2023-11-27 22:18:04 +0100

    Refactor product controller to display cart item count

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,14 +13,17 @@ class ProductController extends AbstractController
     #[Route('/shop', name: 'app_product')]
     public function index(ProductRepository $productRepository, SessionInterface $session): Response
     {
+        //ici on récupère tous les produits
         $products = $productRepository->findAllProducts();
 
+        //ici on récupère ce qu'il y a en session pour le panier
         $cart = $session->get('cart', []);
         $itemCount = 0;
         foreach ($cart as $item) {
             $itemCount += $item['quantity'];
         }
 
+        //ici on affiche la vue en lui passant les produits et le nombre d'articles dans le panier
         return $this->render('product/index.html.twig', [
             'products' => $products,
             'itemCount' => $itemCount,
