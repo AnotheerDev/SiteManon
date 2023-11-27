234443e6977d37baf2adf80976cf394f2d28026e
AnotheerDev <leger.julien94@gmail.com>
2023-11-27 13:35:28 +0100

    Add session handling for cart count

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,11 +11,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProductController extends AbstractController
 {
     #[Route('/shop', name: 'app_product')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, SessionInterface $session): Response
     {
         $products = $productRepository->findAllProducts();
+
+        $cart = $session->get('cart', []);
+        $itemCount = 0;
+        foreach ($cart as $item) {
+            $itemCount += $item['quantity'];
+        }
+
         return $this->render('product/index.html.twig', [
             'products' => $products,
+            'itemCount' => $itemCount,
         ]);
     }
 }
