24fa4d44885fe10e73dafa4e34964de4bb641820
AnotheerDev <leger.julien94@gmail.com>
2023-11-27 21:55:14 +0100

    Refactor category controller to retrieve categories, most clicked topics, and latest topics

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -14,11 +14,17 @@ class CategoryController extends AbstractController
     #[Route('/category', name: 'app_category')]
     public function index(CategoryRepository $categoryRepository, TopicRepository $topicRepository): Response
     {
+        // Récupération de toutes les catégories via le CategoryRepository
         $categories = $categoryRepository->findAllCategories();
+
+        // Récupération des sujets les plus cliqués via le TopicRepository
         $mostClickedTopics = $topicRepository->findMostClickedTopics();
+
+        // Récupération des derniers sujets via le TopicRepository
         $topics = $topicRepository->findLatestTopics();
 
-
+        // Rendu du template Twig pour l'index des catégories
+        // Passage des catégories, des sujets les plus cliqués et des derniers sujets à la vue
         return $this->render('category/index.html.twig', [
             'categories' => $categories,
             'mostClickedTopics' => $mostClickedTopics,
